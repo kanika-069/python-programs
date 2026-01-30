ee47570cc330961cd0de99d6d2d61c59829d7850
kanika-069 <kanikasingh.jab@gmail.com>
2026-01-30 10:45:18 +0530

    python

--- day1.py
+++ day1.py
@@ -1,7 +1,15 @@
 #BAND NAME GENERATOR
+
+#MY CODE
 print("Welcome to the Band Name Generator.")
 print("What's the name of the city you grew up in?")
 c=input()
 print("What's your pet's name?")
 p=input()
 print("Your band name could be",c,p)
+
+#BETTER CODE
+print("Welcome to the Band Name Generator.")
+city=input("Which city did you grow up in?\n")
+pet=input("What is the name of a pet?\n")
+print("Your band name could be: "+city+" "+pet)
